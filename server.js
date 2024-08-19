81140adef468a79e82b96564dae309c53a34d67e
etubom <way2young4u@hotmail.com>
2024-08-19 14:13:27 +0100

    removed comments

--- server.js
+++ server.js
@@ -6,20 +6,6 @@ const app = express()
 //setup static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "index.html"))
-// })
-
-// app.get("/about", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "about.html"))
-// })
-
-// let posts = [
-//   { id: 1, name: 'User One', email: 'user1@mail.com' },
-//   { id: 2, name: 'User Two', email: 'user2@mail.com' },
-//   { id: 3, name: 'User Three', email: 'user3@mail.com' },
-// ]
-
 app.get('/api/users', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'api', 'users.json'))
 })
