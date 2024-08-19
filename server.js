b5b1ba3ae3b782bb25e6bce0efccea1f2dc85743
etubom <way2young4u@hotmail.com>
2024-08-19 13:51:15 +0100

    added server js file config

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
-const express = require("express")
+const express = require('express')
+const path = require('path')
 
 const app = express()
+
+//setup static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"))
+// })
+
+// app.get("/about", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "about.html"))
+// })
+
+// let posts = [
+//   { id: 1, name: 'User One', email: 'user1@mail.com' },
+//   { id: 2, name: 'User Two', email: 'user2@mail.com' },
+//   { id: 3, name: 'User Three', email: 'user3@mail.com' },
+// ]
+
+app.get('/api/users', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'api', 'users.json'))
+})
+
 app.listen(5000, () => console.log(`Server running on port 5000`))
